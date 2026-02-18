37040c25f7533022e311b3dd0fb30d6ad842b2e6
VHall1 <victorhallpsn@hotmail.com>
2026-02-18 20:34:47 +0000

    upgrade dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
